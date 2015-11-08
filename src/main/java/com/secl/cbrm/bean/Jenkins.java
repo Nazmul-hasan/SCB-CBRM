cbf16083962498d16a303e6ebeb693aa29b1e601
Nazmul-hasan <martin.rasel@yahoo.com>
2015-11-08 13:44:00 +0600

    modify

--- src/main/java/com/secl/cbrm/bean/Jenkins.java
+++ src/main/java/com/secl/cbrm/bean/Jenkins.java
@@ -2,4 +2,13 @@ package com.secl.cbrm.bean;
 
 public class Jenkins {
 
+private String gradeID;
+
+	public String getGradeID() {
+		return gradeID;
+	}
+
+	public void setGradeID(String gradeID) {
+		this.gradeID = gradeID;
+	}
 }
